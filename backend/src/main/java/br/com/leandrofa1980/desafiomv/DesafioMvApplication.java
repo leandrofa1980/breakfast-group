bdcf2f61dcf214a3269b8c460f47eebe5bab3e64
Leandro Ferreira <leandrofa1980@gmail.com>
2022-04-06 17:06:35 -0300

    Created project

---
+++ backend/src/main/java/br/com/leandrofa1980/desafiomv/DesafioMvApplication.java
@@ -0,0 +1,13 @@
+package br.com.leandrofa1980.desafiomv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMvApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/leandrofa1980/desafiomv/DesafioMvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandrofa1980.desafiomv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
