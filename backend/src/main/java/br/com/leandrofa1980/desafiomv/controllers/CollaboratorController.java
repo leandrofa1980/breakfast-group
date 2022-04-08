1f51799c9f9572bc57b8c6a294849d745725f998
Leandro Ferreira <leandrofa1980@gmail.com>
2022-04-08 09:59:02 -0300

    Dependency NativeQuery, deploy Heroku

--- backend/src/main/java/br/com/leandrofa1980/desafiomv/controllers/CollaboratorController.java
+++ backend/src/main/java/br/com/leandrofa1980/desafiomv/controllers/CollaboratorController.java
@@ -27,42 +27,40 @@ public class CollaboratorController {
 
 	@Autowired
 	private CollaboratorService service;
-	
+
 	@GetMapping
-	public ResponseEntity<Page<CollaboratorDTO>> findAll(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
+	public ResponseEntity<Page<CollaboratorDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "5") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			){
-		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction) ,orderBy);
+
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		Page<CollaboratorDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<CollaboratorDTO> findById(@PathVariable Long id){
+	public ResponseEntity<CollaboratorDTO> findById(@PathVariable Long id) {
 		CollaboratorDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<CollaboratorDTO> insert(@RequestBody	CollaboratorDTO dto){
+	public ResponseEntity<CollaboratorDTO> insert(@RequestBody CollaboratorDTO dto) {
 		dto = service.insert(dto);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(dto.getId()).toUri();		
-		return ResponseEntity.created(uri).body(dto);		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<CollaboratorDTO> update(@PathVariable Long id, @RequestBody CollaboratorDTO dto){
+	public ResponseEntity<CollaboratorDTO> update(@PathVariable Long id, @RequestBody CollaboratorDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<CollaboratorDTO> delete(@PathVariable Long id){
+	public ResponseEntity<CollaboratorDTO> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
